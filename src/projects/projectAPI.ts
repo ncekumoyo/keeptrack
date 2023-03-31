943906c759b78345fea448faebc94b8d0bfe4832
ncekumoyo <nhmoyo7@gmail.com>
2023-03-31 14:45:37 +0200

    up to custom hooks

--- src/projects/projectAPI.ts
+++ src/projects/projectAPI.ts
@@ -51,17 +51,15 @@ function convertToProjectModel(item: any): Project {
 
 const projectAPI = {
   get(page = 1, limit = 20) {
-    return (
-      fetch(`${url}?_page=${page}&_limit=${limit}&_sort=name`)
-        //.then(delay(600))
-        .then(checkStatus)
-        .then(parseJSON)
-        .then(convertToProjectModels)
-        .catch((error: TypeError) => {
-          console.log("log client error " + error);
-          throw new Error("There was an error retrieving the projects. Please try again.");
-        })
-    );
+    return fetch(`${url}?_page=${page}&_limit=${limit}&_sort=name`)
+      .then(delay(2000))
+      .then(checkStatus)
+      .then(parseJSON)
+      .then(convertToProjectModels)
+      .catch((error: TypeError) => {
+        console.log("log client error " + error);
+        throw new Error("There was an error retrieving the projects. Please try again.");
+      });
   },
   put(project: Project) {
     return fetch(`${url}/${project.id}`, {
